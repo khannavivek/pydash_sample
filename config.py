cc35b2f58431e154a68e0870fd87087c58e8c7e6
khannavivek <vivekkhanna987@gmail.com>
2023-10-02 16:54:05 +0000

    code restructure

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-host="0.0.0.0"
-port=7777
-threaded=True
-
 dev_tools_hot_reload=True
 dev_tools_hot_reload_interval=4000
 dev_tools_hot_reload_max_retry=30
+
+host="0.0.0.0"
+port=7777
+threaded=True
